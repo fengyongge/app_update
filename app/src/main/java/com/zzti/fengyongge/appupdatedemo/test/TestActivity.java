1c0bc28f1b1cb1e5e41c166fb231bce652d1a3ad
fsuper <917386389@qq.com>
2018-01-15 15:06:10 +0800

    git first

---
+++ app/src/main/java/com/zzti/fengyongge/appupdatedemo/test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.zzti.fengyongge.appupdatedemo.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zzti.fengyongge.appupdatedemo.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
+}
